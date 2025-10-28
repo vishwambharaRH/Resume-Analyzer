4d6e63f93f53a77726a8c67269094e7a19afa0de
Vishwambhara R Hebbalalu <vishwambharah@gmail.com>
2025-10-28 23:17:45 +0530

    initial develop commit for FR004

---
+++ vish_changes/parser.py
@@ -0,0 +1,10 @@
+import string
+
+class Parser:
+    def __init__(self, text):
+        self.text = text
+
+    def parse(self):
+        # Basic parsing: lowercasing and removing punctuation
+        print("Hello parse!")
+
